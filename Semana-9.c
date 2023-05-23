27c79da5307120e3214d40bc4c541784c7d281fd
Esqueleto27 <matheofloresloor@gmail.com>
2023-05-23 12:45:49 -0500

    Función para calcular la matriz transpuesta

--- Semana-9.c
+++ Semana-9.c
@@ -5,12 +5,20 @@
 int generarAleatorio(int min, int max) {
     return min + rand() % (max - min + 1);
     }
-    
+
 void imprimirMatriz(int **matriz, int filas, int columnas) {
     for (int i = 0; i < filas; i++) {
         for (int j = 0; j < columnas; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
+    }
+}
+
+    void calcularTranspuesta(int **matriz, int **transpuesta, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            transpuesta[j][i] = matriz[i][j];
+        }
     }
 }

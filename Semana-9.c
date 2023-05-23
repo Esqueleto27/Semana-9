214c5ccfe259d58d201aa12a5957fb9229747de9
Esqueleto27 <matheofloresloor@gmail.com>
2023-05-23 12:45:01 -0500

    Función para imprimir una matriz

--- Semana-9.c
+++ Semana-9.c
@@ -4,3 +4,13 @@
 
 int generarAleatorio(int min, int max) {
     return min + rand() % (max - min + 1);
+    }
+    
+void imprimirMatriz(int **matriz, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
